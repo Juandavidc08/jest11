c2741c52f8224225f5b756f1f34fcbbe5a7436f5
Juandavid08 <jdavidcamelo94@gmail.com>
2024-03-27 10:16:00 +0100

    Add event Listeners when click

--- scripts/game.js
+++ scripts/game.js
@@ -3,13 +3,25 @@ let game = {
     currentGame: [],
     playerMoves: [],
     score: 0,
+    turnNumber: 0,
     choices: ["button1", "button2", "button3", "button4"]
 };
 
 function newGame() {
+    game.score = 0;
     game.currentGame = [];
     game.playerMoves = [];
-    game.score = 0;
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
     showScore();
     addTurn();
 }
@@ -17,7 +29,7 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns();
+    showTurns();
 }
 
 function lightsOn(circ) {
@@ -31,4 +43,14 @@ function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.lenght){
+            clearInterval(turns);
+        }
+    }, 800);
+}
+module.exports = { game, newGame, showScore, addTurn, lightsOn , showTurns };
